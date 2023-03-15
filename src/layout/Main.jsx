1bf5dc801ceafab3c6b5756f306cfe03784ac154
a.sotnichenko <haliivi@mail.ru>
2023-03-15 12:40:44 +0700

    1) Added 2 classes to index.css. 2) Overwrite logic preloader. 3) Write component Preloader. 4) Write component Search. 5) Import component and write logic.

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
+import {Search} from '../components/Search'
 import {Movies} from '../components/Movies'
+import { Preloader } from '../components/Preloader'
 
 class Main extends React.Component {
     constructor (props) {
         super(props);
-        this.state = {movies: []}
+        this.state = {
+            movies: [],
+            loading: true,
+        }
     }
 
     componentDidMount () {
         fetch ('http://www.omdbapi.com/?apikey=7b4ee842&s=matrix')
         .then(response => response.json())
-        .then(data => this.setState({movies: data.Search}))
+        .then(data => this.setState({movies: data.Search, loading: false,}))
+    }
+
+    searchMovies = (str, type='all') => {
+        this.setState({loading: true,})
+        fetch (`http://www.omdbapi.com/?apikey=7b4ee842&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search, loading: false,}))
     }
 
     render () {
-        const {movies} = this.state;
+        const {movies, loading} = this.state;
         return (
             <main className="container content">
+                <Search searchMovies={this.searchMovies} />
                 {
-                    movies.length ? (
+                    loading ? (
+                        <Preloader />
+                    ) : (
                         <Movies movies={movies} />
-                    ) : <h5>Loading...</h5>
+                    )
                 }
-                
             </main>
         )
     }
