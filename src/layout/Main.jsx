91971ba37547531fa911b1feab3c4bbeec694aa3
a.sotnichenko <haliivi@mail.ru>
2023-03-15 12:46:32 +0700

    1) Added environment variable.

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,8 @@ import {Search} from '../components/Search'
 import {Movies} from '../components/Movies'
 import { Preloader } from '../components/Preloader'
 
+const API_KEY = process.env.REACT_APP_API_KEY
+
 class Main extends React.Component {
     constructor (props) {
         super(props);
@@ -13,14 +15,14 @@ class Main extends React.Component {
     }
 
     componentDidMount () {
-        fetch ('http://www.omdbapi.com/?apikey=7b4ee842&s=matrix')
+        fetch (`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
         .then(response => response.json())
         .then(data => this.setState({movies: data.Search, loading: false,}))
     }
 
     searchMovies = (str, type='all') => {
         this.setState({loading: true,})
-        fetch (`http://www.omdbapi.com/?apikey=7b4ee842&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+        fetch (`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
         .then(response => response.json())
         .then(data => this.setState({movies: data.Search, loading: false,}))
     }
