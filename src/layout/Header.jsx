82e92204312e05a1ca57b9828b7cb69ce5ff652e
a.sotnichenko <haliivi@mail.ru>
2023-03-17 13:23:23 +0700

    Refactoring.

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,7 @@ function Header () {
     return (
         <nav className="green lighten-2">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">React Movies</a>
+                <a href="#!" className="brand-logo">React Movies</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="https://github.com/haliivi/react_movies_search">Repo</a></li>
                 </ul>
